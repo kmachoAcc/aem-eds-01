802c11737c5ecdd88433eca037a42b1b03ff82f0
Luis Eduardo Camacho Zúñiga <lec3689@eid.utexas.edu>
2026-07-10 16:04:49 -0600

    Update accordions.js

--- blocks/accordions/accordions.js
+++ blocks/accordions/accordions.js
@@ -1,16 +1,7 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-
-
-/**
- * Toggles all nav sections
- * @param {Element} sections The container element
- * @param {Boolean} expanded Whether the element should be expanded or collapsed
- */
 function toogleAccordion(row) {
   const openDetails = document.querySelectorAll('.accordions-block.is-open');
 
-  
-  openDetails.forEach(detail => {
+  openDetails.forEach((detail) => {
     detail.classList.remove('is-open');
   });
 
@@ -21,9 +12,9 @@ export default function decorate(block) {
   console.log(block);
   /* change to ul, li */
   [...block.children].forEach((row) => {
-    let accordionTitle = row.firstElementChild;
-    let accordionDescription = row.lastElementChild;
-   
+    const accordionTitle = row.firstElementChild;
+    const accordionDescription = row.lastElementChild;
+
     accordionTitle.className = 'accordions-block-title';
     accordionDescription.className = 'accordions-block-description';
     row.className = 'accordions-block';
